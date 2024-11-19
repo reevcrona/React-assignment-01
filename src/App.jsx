9b90077f4c3e2432c0dc6cba1133556fc97e73a3
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-11-19 05:07:15 +0100

    added bioshock the collection images

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import GameCard from "./GameCard";
 import Bs1Art from "./assets/bs1-box-art.jpg";
 import Bs2Art from "./assets/bs2-box-art.jpg";
 import BsIArt from "./assets/bsi-box-art.jpg";
+import BstcArt from "./assets/bstc-box-art.jpg";
 
 import Bs1Bg from "./assets/test-bg.jpg";
 import Bs2Bg from "./assets/bs2-bg.jpg";
 import BsIBg from "./assets/bsI-bg.jpg";
+import BstcBg from "./assets/bstc-bg.jpg";
+
 function App() {
   return(
    <div>
@@ -13,6 +16,7 @@ function App() {
         <GameCard gameTitle ="Bioshock" gameArt = {Bs1Art} gameClass ="bs1" bgImage = {Bs1Bg} />
         <GameCard gameTitle ="Bioshock 2" gameArt = {Bs2Art} gameClass ="bs2" bgImage ={Bs2Bg}/>
         <GameCard gameTitle ="Bioshock Infinite" gameArt = {BsIArt} gameClass ="bsI" bgImage ={BsIBg} />
+        <GameCard gameArt = {BstcArt} gameClass ="bstc" bgImage = {BstcBg} />
       </div>
     
     </div>
